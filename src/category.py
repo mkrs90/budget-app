110fd6cb03479e6255d2cb23c921ce91aacf3bda
mkrs90 <katy.simpson@student.awesomeinc.org>
2023-03-16 15:58:32 +0000

    deposite and withdraw methods

--- src/category.py
+++ src/category.py
@@ -1,11 +1,35 @@
 class Category:
     def __init__(self):
-        pass
-    def deposit(self):
-        pass
+        self.type = "type"
+        self.ledger = []
+        self.amount = 0
+        self.description = ""
+        self.total = 0
+
+    def deposit(self, pAmount, pdescription):
+        emptyStr = " "
+        if description == "":
+            self.ledger.append({ "amount": pAmount, "description": emptyStr })
+        else:
+            self.ledger.append({"amount": pAmount, "description": pdescription})
+
+    def withdraw(self, nAmount, ndescription):
+        if nAmount > self.total:
+            return False
+        else:
+            self.ledger.append({"amount": nAmount, "description": ndescription})
+            return True
+
     def get_balance(self):
+        getBalance = self.ledger.values()
+        balance = eval(getBalance)
+        return balance
+
+    def transfer(self, amount, type):
         pass
-    def transfer(self):
+
+    def check_funds(self, amount, type):
         pass
-    def check_funds(self):
+
+    def __str__(self):
         pass
--- src/tests/test_sample.py
+++ src/tests/test_sample.py
@@ -1,4 +1,10 @@
+from category import Category
+
 def test_one():
     assert True
 
 # You can write tests here or create new files in this directory with the name test_[something].py
+
+def test_deposit():
+    test = Category(10, "for brunch")
+    assert True
